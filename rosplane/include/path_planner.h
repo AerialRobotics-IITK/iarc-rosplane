73b8d9828bc38bab6a524ad6e9c4fa33bd7ce993
kpiyush25 <piyushk@iitk.ac.in>
2021-02-03 00:24:39 +0530

    Make computeForwardPoints, computeBackwardPoints public

--- rosplane/include/path_planner.h
+++ rosplane/include/path_planner.h
@@ -5,13 +5,12 @@
 class RosplaneDubins {
   public:
     void init(ros::NodeHandle& nh);
+    void computeForwardPoints();
+    void computeBackwardPoints();
     std::vector<float> wps;
     std::vector<float> backward_wps;
 
   private:
-    void computeForwardPoints();
-    void computeBackwardPoints();
-
     double left_y;
     double right_y;
     double r;
